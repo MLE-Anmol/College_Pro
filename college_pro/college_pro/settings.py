7319e2bc476e24199438b467f201aca106233425
MLE-Anmol <anmoluk1988@gmail.com>
2021-12-31 00:05:53 +0530

    student_model_migration

--- college_pro/college_pro/settings.py
+++ college_pro/college_pro/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'college_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lfhoolcy',
+        'USER': 'lfhoolcy',
+        'PASSWORD': 'DfNzMl4xQhtzI2QR1ReQ2VPA7Fy0-k2x',
+        'HOST': 'castor.db.elephantsql.com'
     }
 }
 
--- college_pro/student/models.py
+++ college_pro/student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    stu_fname = models.CharField(max_length=20)
+    stu_lname = models.CharField(max_length=20)
+    stu_id = models.CharField(max_length=10)
+    program = models.CharField(max_length=15)
+    
